bfb4c1eda4879f0116a5c2703a050a9472e35244
Nancy <nancy@openagent.com.au>
2017-09-19 10:45:20 +1000

    convert response body to hashmap

--- src/test/java/com/students/tests/OpenAgentUrlEncodedTests.java
+++ src/test/java/com/students/tests/OpenAgentUrlEncodedTests.java
@@ -1,11 +1,10 @@
 package com.students.tests;
 
 import static com.jayway.restassured.RestAssured.given;
+import java.util.Map;
+import java.util.HashMap;
 import org.json.*;
 import static org.junit.Assert.assertEquals;
-
-import java.util.ArrayList;
-
 import org.json.JSONException;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -24,7 +23,6 @@ public class OpenAgentUrlEncodedTests {
 	
 	@Test
 	public void createVisitor() throws JSONException{
-
 		Response response = given()
 				.header("Content-Type", "application/x-www-form-urlencoded")
 				.formParam("munchkin","sadfxesce")
@@ -35,24 +33,37 @@ public class OpenAgentUrlEncodedTests {
 	}
 	
 	@Test
-	public void updateVisitor() throws JSONException{
-
+	public void updateVisitor(){
+        String munchParam= "asdf23432";
 		Response response = given()
 				.header("Content-Type", "application/x-www-form-urlencoded")
-				.formParam("munchkin","sa8dfxe")
+				.formParam("munchkin",munchParam)
 				.request()				
 		        .put("/35039");
-		
-		// Find how to convert string to hashmap
-//		String datas[] =response.body().jsonPath().getString("data").split(",");
-//		String munchkin = datas[2];
-//		System.out.println(munchkin);
-		
-		Long statusCode = new Long(response.statusCode());
-
-		if(statusCode.toString().equals("200")){		
+		assertParamWithResponse(response,munchParam,"munchkin");
+	}
 	
+	
+	public Map<String, String> turnResponseToHashmap(Response response){
+		Map<String, String> responseBody= new HashMap<String,String>();
+		String data=response.body().jsonPath().getString("data");
+		data = data.substring(1,data.length()-1); // Remove bracket
+		String[] resData = data.split(",");
+		for(String value : resData)
+		{
+			String[] entry = value.split(":");
+			responseBody.put(entry[0].trim(), entry[1].trim());
+		}
+		System.out.println(responseBody);
+		return responseBody;	
+	}
+	
+	public void assertParamWithResponse(Response response, String param, String resKey){
+		Long statusCode = new Long(response.statusCode());
+		if(statusCode.toString().equals("200")){	
+			String resValue = turnResponseToHashmap(response).get(resKey);
+			assertEquals(param, resValue);
 		}
 	}
-
+	
 }
